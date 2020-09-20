9cc02f996cf2573fd5414a9775213e00f47d70fe
cabralesCarrillo <luis.cabrales.2016@upb.edu.co>
2020-09-20 16:57:43 -0500

    LAB 7

---
+++ src/main/java/com/mycompany/holamundo/Main.java
@@ -0,0 +1,12 @@
+
+package com.mycompany.holamundo;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Main {
+      public static void main(String[] args){
+          System.out.println("Hola Mundo");
+      }
+}
